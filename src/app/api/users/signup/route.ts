817fabcfe84c422ec2382f8f11c77343b2700abc
Muhammad-Haram <haramh643@gmail.com>
2024-08-12 12:32:01 +0500

    signIn complete

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -7,7 +7,7 @@ import { sentEmail } from "@/helpers/mailer";
 connect();
 export async function POST(request: NextRequest) {
   try {
-    const reqBody = request.json();
+    const reqBody = await request.json();
     const { username, email, password } = reqBody;
     console.log(reqBody);
     const user = await User.findOne({ email });
--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,25 +1,39 @@
+import User from "@/models/userModel";
 import nodemailer from "nodemailer";
+import bcryptjs from "bcryptjs";
 
 export const sentEmail = async ({ email, emailType, userId }: any) => {
+
   try {
     //TODO: configure mail for usage
 
-    const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false,
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, { verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000 })
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, { forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000 })
+    }
+
+    var transporter = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
       auth: {
-        user: "maddison53@ethereal.email",
-        pass: "jn7jnAPss4f63QBp6D",
-      },
+        user: "20e25c77b71b71",
+        pass: "75a6ebf07fe6ff"
+      }
     });
+
+    const verifyHtml = `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"} or copy and paste the link below in your browser <br/>
+    ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+    </p>`
 
     const mailOptions = {
       from: "haramh643@gmail.com",
       to: email,
       subject:
         emailType === "VERIFY" ? "Verified your email" : "Reset your password",
-      html: "<b>Hello world?</b>",
+      html: verifyHtml,
     };
 
     const mailResponse = await transporter.sendMail(mailOptions);
