d3d44aa8df8f22684b0d9dd66c16be4e0ddf89bb
Muhammad-Haram <haramh643@gmail.com>
2024-08-15 11:20:26 +0500

    verify email

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -7,8 +7,22 @@ connect();
 export async function POST(request: NextRequest) {
   try {
     const reqBody = await request.json();
-    const {token} = reqBody;
+    const { token } = reqBody;
     console.log(token);
+
+    const user = await User.findOne({ verifyToken: token, verifyTokenExpiry: { $gt: Date.now } });
+
+    if (!user) {
+      return NextResponse.json({ error: "invalid token/user not found" }, { status: 400 });
+    }
+
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+
+    await user.save();
+    return NextResponse.json({ message: "email verified successfully" }, { status: 200 });
+
 
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
